f0194f7c880e7fd8748587e91f96d693c0444465
ZulfaliaPutri <lia.putricandrawati@gmail.com>
2021-12-11 18:38:18 +0800

    [feature] add loading on sending data to backend

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   console.log("[INFO] script started");
   $("#button-input").click(function (e) {
     e.preventDefault();
+    $("#result-loading").removeClass("hide");
     var text = $("#text-input").val();
     $.ajax({
       url: "http://localhost:8080/predict",
@@ -11,6 +12,7 @@ $(document).ready(function () {
       complete: function (data, code) {
         data.then((data) => {
           data = JSON.parse(data);
+          $("#result-loading").addClass("hide");
           if (data[0] == 1) {
             $("#result-hate").removeClass("hide");
             $("#result-not-hate").addClass("hide");
